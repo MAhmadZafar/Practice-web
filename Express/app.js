ee45da8322961ac12088ec4400b53ad01fc2bcf0
Ahmad <ahmadzafar616@gmail.com>
2022-07-18 16:40:21 +0500

    Using pug

--- Express/app.js
+++ Express/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const fs = require("fs")
 const path = require("path")
 const app = express();
+const linereader= require("line-reader")
 const port=80;
 
 app.use('/static',express.static('static'))
-
-
+app.use(express.urlencoded())
 
 app.set("view engine","pug")
 
@@ -14,6 +15,32 @@ app.set("views",path.join(__dirname,"views"))
 app.get("/",(req,res)=>{
     const message="ahmad is a good boy"
     const parameters={'title':"Ahmad",'message':message}
+    res.status(200).render("index.pug",parameters)
+})
+
+
+global.flag=true;
+
+app.post("/",(req,res)=>{
+    fame=req.body
+    global.Name=JSON.stringify(fame)
+    Name="\n"+Name
+    linereader.eachLine("index.txt",(line,last)=>{
+        // console.log(line)
+        let obj= JSON.parse(line)
+        if(fame.Name==obj.Name){
+            console.log("customer already exist")
+            global.flag=false
+            global.Name=""
+        }
+            console.log(flag)
+            console.log(obj)
+            if(flag){
+                fs.appendFileSync("index.txt",Name,"utf-8",()=>{})
+            }
+        }) 
+        const message="Form is submitted successfully"
+    const parameters={'msg':fame.Name}
     res.status(200).render("index.pug",parameters)
 })
 
